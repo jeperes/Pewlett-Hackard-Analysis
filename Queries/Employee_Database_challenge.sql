627a8c3f832ca608004aa2790816bdeb78410676
Jeniffer Peres <95551734+jeperes@users.noreply.github.com>
2022-02-12 17:03:35 -0700

    Adding Employee_Database_challenge.sql file

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -37,3 +37,22 @@ SELECT *
 FROM retiring_titles;
 
 
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.titles INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+							AND de.to_date = '9999-01-01')
+ORDER BY e.emp_no,
+	ti.to_date DESC;
+	
+SELECT *
+FROM mentorship_eligibilty;
+
+
